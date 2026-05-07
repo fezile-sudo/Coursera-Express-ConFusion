03bdb135b786009d1a7bd2479a0cd9d9d56b4b94
Fezile Gulwa <fezilegulwa@gmail.com>
2026-05-07 23:17:37 +0200

    Express Session Part 2

---
+++ models/dist/user.dev.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var User = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  admin: {
+    type: Boolean,
+    "default": false
+  }
+});
+module.exports = mongoose.model('User', User);
+//# sourceMappingURL=user.dev.js.map
--- routes/dist/users.dev.js
+++ routes/dist/users.dev.js
@@ -1,94 +1,94 @@
+"use strict";
+
 var express = require('express');
-const bodyParser = require('body-parser');
+
+var bodyParser = require('body-parser');
+
 var User = require('../models/user');
 
-
 var router = express.Router();
-
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
-
-
-
-router.post('/signup', (req, res, next) => {
-  User.findOne({username: req.body.username})
-  .then((user) => {
-    if(user != null) {
+router.post('/signup', function (req, res, next) {
+  User.findOne({
+    username: req.body.username
+  }).then(function (user) {
+    if (user != null) {
       var err = new Error('User ' + req.body.username + ' already exists!');
       err.status = 403;
       next(err);
-    }
-    else {
+    } else {
       return User.create({
         username: req.body.username,
-        password: req.body.password});
+        password: req.body.password
+      });
     }
-  })
-  .then((user) => {
+  }).then(function (user) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.json({status: 'Registration Successful!', user: user});
-  }, (err) => next(err))
-  .catch((err) => next(err));
+    res.json({
+      status: 'Registration Successful!',
+      user: user
+    });
+  }, function (err) {
+    return next(err);
+  })["catch"](function (err) {
+    return next(err);
+  });
 });
-
-router.post('/login', (req, res, next) => {
-
-  if(!req.session.user) {
+router.post('/login', function (req, res, next) {
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
-    
+
     if (!authHeader) {
       var err = new Error('You are not authenticated!');
       res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       return next(err);
     }
-  
+
     var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
     var username = auth[0];
     var password = auth[1];
-  
-    User.findOne({username: username})
-    .then((user) => {
+    User.findOne({
+      username: username
+    }).then(function (user) {
       if (user === null) {
         var err = new Error('User ' + username + ' does not exist!');
         err.status = 403;
         return next(err);
-      }
-      else if (user.password !== password) {
+      } else if (user.password !== password) {
         var err = new Error('Your password is incorrect!');
         err.status = 403;
         return next(err);
-      }
-      else if (user.username === username && user.password === password) {
+      } else if (user.username === username && user.password === password) {
         req.session.user = 'authenticated';
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-        res.end('You are authenticated!')
+        res.end('You are authenticated!');
       }
-    })
-    .catch((err) => next(err));
-  }
-  else {
+    })["catch"](function (err) {
+      return next(err);
+    });
+  } else {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('You are already authenticated!');
   }
-})
-
-router.get('/logout', (req, res) => {
+});
+router.get('/logout', function (req, res) {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
     res.redirect('/');
-  }
-  else {
+  } else {
     var err = new Error('You are not logged in!');
     err.status = 403;
     next(err);
   }
 });
-
 module.exports = router;
+//# sourceMappingURL=users.dev.js.map
